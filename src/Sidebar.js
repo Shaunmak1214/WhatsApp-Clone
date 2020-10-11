c02733054e9b04beb6099cd410b2f485997461b5
Shaunmak1214 <shaonmak@gmail.com>
2020-10-11 18:19:37 +0800

    Halfway on sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Sidebar.css'
-import { Avatar } from '@material-ui/core';
+import { Avatar, IconButton } from '@material-ui/core';
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
 
 function Sidebar() {
     return (
@@ -11,14 +12,28 @@ function Sidebar() {
             <div className="sidebar__header">
                 <Avatar />
                 <div className="sidebar__headerIcon">
+                {/* <IconButton> to make the icon clickable */}
+                <IconButton>
                     <DonutLargeIcon />
+                </IconButton>
+
+                <IconButton>
                     <ChatIcon />
+                </IconButton>
+ 
+                <IconButton>
                     <MoreVertIcon />
+                </IconButton>
                 </div>
             </div>
 
             <div className="sidebar__search">
 
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat" type="text"></input> 
+                </div>
+                
             </div>
             
             <div className="sidebar__chats">
